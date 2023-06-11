6cbdb6761757895ce7b54b8dc2fd0a6c57374b8c
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-06-11 22:49:56 +0200

    unitest should get two books

--- LibraryBackend.Tests/UnitTest.cs
+++ LibraryBackend.Tests/UnitTest.cs
@@ -15,32 +15,36 @@ namespace LibraryBackend.Tests
     { new Book{
         Title="title1",
         Author="author1"
+    },new Book{
+      Title="title2",
+      Author="author2"
     }
     };
 
     public UnitTest()
     {
-        _mockBookRepository = new Mock<IBookRepository>();
-        _bookController = new BookController (_mockBookRepository.Object);
+      _mockBookRepository = new Mock<IBookRepository>();
+      _bookController = new BookController(_mockBookRepository.Object);
 
     }
 
 
     [Fact]
-    public async void should_get_one_Book()
+    public async void should_get_two_Books()
     {
       // arrange
       _mockBookRepository.Setup(elementMock => elementMock.GetAllBooksAsync()).ReturnsAsync(mockData);
-    
-        // Act
-        var result = await _bookController.GetBook();
-       
-       
-        // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var books = Assert.IsAssignableFrom<IEnumerable<Book>>(okResult.Value);
-        Assert.Equal(1,books?.Count());
-        Assert.Equal("title1", books?.ElementAt(0).Title);
+
+      // Act
+      var result = await _bookController.GetBook();
+
+
+      // Assert
+      var okResult = Assert.IsType<OkObjectResult>(result.Result);
+      var books = Assert.IsAssignableFrom<IEnumerable<Book>>(okResult.Value);
+      Assert.Equal(2, books?.Count());
+      Assert.Equal("title1", books?.ElementAt(0).Title);
+      Assert.Equal("author2", books?.ElementAt(1).Author);
 
     }
   }
