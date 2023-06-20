b10358c2513133d0d16015cec0be231989e18996
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-06-20 18:52:25 +0200

    delete some comment from BookController

--- LibraryBackend/Controllers/BookController.cs
+++ LibraryBackend/Controllers/BookController.cs
@@ -90,70 +90,7 @@ namespace LibraryBackend.Controllers
       return Ok(bookToModify); 
     }
 
-
-
-
     /* 
-            
-
-            // PUT: api/Book/5
-            // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-            [HttpPut("{id}")]
-            public async Task<IActionResult> PutBook(int id, Book book)
-            {
-                if (id != book.BookId)
-                {
-                    return BadRequest();
-                }
-
-                _context.Entry(book).State = EntityState.Modified;
-
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookExists(id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
-                return NoContent();
-            }
-
-            // POST: api/Book
-            // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-            [HttpPost]
-            public async Task<ActionResult<Book>> PostBook(Book book)
-            {
-                _context.Book.Add(book);
-                await _context.SaveChangesAsync();
-
-                return CreatedAtAction("GetBook", new { id = book.BookId }, book);
-            }
-
-            // DELETE: api/Book/5
-            [HttpDelete("{id}")]
-            public async Task<IActionResult> DeleteBook(int id)
-            {
-                var book = await _context.Book.FindAsync(id);
-                if (book == null)
-                {
-                    return NotFound();
-                }
-
-                _context.Book.Remove(book);
-                await _context.SaveChangesAsync();
-
-                return NoContent();
-            }
-
             private bool BookExists(int id)
             {
                 return _context.Book.Any(e => e.BookId == id);
