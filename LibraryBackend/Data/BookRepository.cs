44597b5d84004abfddfba5bf56746963dfb3743b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-06-28 20:21:46 +0200

    implement a book repository

--- LibraryBackend/Data/BookRepository.cs
+++ LibraryBackend/Data/BookRepository.cs
@@ -3,52 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LibraryBackend.Data
 {
-  public class BookRepository : IBookRepository
+  public class BookRepository : Repository<Book>, IBookRepository
   {
     private readonly MyLibraryContext _context = default!;
 
-    public BookRepository(MyLibraryContext context) => _context = context;
-
-    public async Task<IEnumerable<Book>> GetAllBooksAsync()
+    public BookRepository(MyLibraryContext context) : base(context)
     {
-
-      return await _context.Book.Include(x => x.Opinions).ToListAsync();
-
+      _context = context;
     }
 
-    public async Task<Book> CreateBook(Book newBook)
-    {
-     /*  var newBook = new Book
-      {
-        Title = title,
-        Author = author
-      }; */
-
-      _context.Book.Add(newBook);
-      await _context.SaveChangesAsync();
-      return newBook;
-    }
-
-    public async Task DeleteBook(Book bookToDelete)
-    {
-      _context.Book.Remove(bookToDelete);
-      await _context.SaveChangesAsync();
-
-    }
-
-
-    public async Task<Book?> GetBookByIdAsync(int id)
-    {
-      return await _context.Book.Include(x => x.Opinions).Where(book => book.BookId == id).FirstOrDefaultAsync();
-
-    }
-
-    public Book GetBookByTitle(int id, string title)
-    {
-      throw new NotImplementedException();
-    }
-
-    public Book UpdateBook(Book book, string title, string author)
+     public Book UpdateBook(Book book, string title, string author)
     {
       book.Author = author;
       book.Title = title;
@@ -58,6 +22,6 @@ namespace LibraryBackend.Data
 
       return updatedBook.Entity;
 
-    }
+    } 
   }
 }
