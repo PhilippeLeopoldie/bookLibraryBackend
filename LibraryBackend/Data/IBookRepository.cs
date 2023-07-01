c905ffa8015f08b92b2014120ef46c3aba7bcb69
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-07-01 09:21:14 +0200

    update IBookRepository

--- LibraryBackend/Data/IBookRepository.cs
+++ LibraryBackend/Data/IBookRepository.cs
@@ -4,6 +4,6 @@ namespace LibraryBackend.Data
 {
   public interface IBookRepository : IRepository<Book>
   {
-     Book UpdateBook(Book book, string element1, string author);
+     Book UpdateBook(Book book, string title, string author);
   }
 }
