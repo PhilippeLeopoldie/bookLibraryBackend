026a95a090c6d3a2d6c17e029f84c78947aa5cbe
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-07-01 09:20:27 +0200

    update IRepository made it fully generic

--- LibraryBackend/Data/Repository.cs
+++ LibraryBackend/Data/Repository.cs
@@ -11,65 +11,36 @@ namespace LibraryBackend.Data
 
     public Repository(MyLibraryContext context)
     {
-
       _context = context;
       _entities = context.Set<T>();
     }
 
-    public async Task<IEnumerable<T>> GetAllBooksAsync()
+    public async Task<IEnumerable<T>> GetAllAsync()
     {
-
       return await _entities.ToListAsync();
-
     }
 
-    public async Task<T> CreateBook(T entity)
-    {
-      /*  var newBook = new Book
-       {
-         Title = title,
-         Author = author
-       }; */
-
+    public async Task<T> Create(T entity)
+    {  
       _entities.Add(entity);
       await _context.SaveChangesAsync();
       return entity;
     }
 
-    public async Task DeleteBook(T entityToDelete)
+    public async Task Delete(T entityToDelete)
     {
       _context.Remove(entityToDelete);
       await _context.SaveChangesAsync();
-
     }
 
-
-    public async Task<T?> GetBookByIdAsync(int id)
+    public async Task<T?> GetByIdAsync(int id)
     {
-      //return await _context.Book.Include(x => x.Opinions).Where(book => book.BookId == id).FirstOrDefaultAsync();
        return await _entities.FirstOrDefaultAsync(x => x.Id == id);
     }
-
-    /* public Book GetBookByTitle(int id, string title)
-    {
-      throw new NotImplementedException();
-    } */
 
     public async Task<T?> FindByConditionAsync(Expression<Func<T,bool>> predicate)
     {
       return await _context.Set<T>().FirstOrDefaultAsync(predicate);
     }
-
-    /* public Book UpdateBook(Book book, string title, string author)
-    {
-      book.Author = author;
-      book.Title = title;
-
-      var updatedBook = _context.Book.Update(book);
-      _context.SaveChanges();
-
-      return updatedBook.Entity;
-
-    } */
   }
 }
