a9ec490356fd2abbb3d4407493df911dfe9f12c1
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-08-28 11:25:03 +0200

    Delete Book and Opinion Interfaces

--- LibraryBackend/Data/IOpinionRepository.cs
+++
@@ -1,9 +0,0 @@
-using LibraryBackend.Models;
-
-namespace LibraryBackend.Data
-{
-  public interface IOpinionRepository : IRepository<Opinion>
-  {
-    Opinion UpdateOpinion(Opinion opinion, string view, string userName, int like);
-  }
-}
