446e9e3aab26dd9e8f57d29ec26e3e573c228d7f
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-01 23:57:07 +0200

    Implemente BookDtoResponse

---
+++ LibraryBackend/Models/BookDtoResponse.cs
@@ -0,0 +1,10 @@
+using LibraryBackend.Models;
+
+namespace LibraryBackend.Models
+{
+  public class BookDtoResponse 
+  {
+    public Book? Book {get; set;}
+    public string? RequestedAt {get; set;}
+  }
+}
