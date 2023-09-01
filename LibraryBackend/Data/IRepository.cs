5298207080164c0e0aacafef5879b778be48ce3f
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-02 01:06:57 +0200

    Refactor by adding update generic in IRepository

--- LibraryBackend/Data/IRepository.cs
+++ LibraryBackend/Data/IRepository.cs
@@ -8,11 +8,13 @@ namespace LibraryBackend.Data
     Task<IEnumerable<T>> GetAllAsync();
 
     Task<T?> GetByIdAsync(int id);
-   
-    Task<T?> FindByConditionAsync(Expression<Func<T,bool>> predicate);
-    
+
+    Task<T?> FindByConditionAsync(Expression<Func<T, bool>> predicate);
+
     Task<T> Create(T newEntity);
-    
+
+    Task<T> Update(T entity);
+
     Task Delete(T entity);
 
   }
