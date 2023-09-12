3aaffa91c2c9725c2c3997611bc377585bd68856
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-12 14:52:42 +0200

    Modified the Opinion field Like into Rate

---
+++ LibraryBackend/Migrations/20230912124341_Modified_The_Opinion_Field_Like_Into_Rate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modified_The_Opinion_Field_Like_Into_Rate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Like",
+                table: "Opinion",
+                newName: "Rate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rate",
+                table: "Opinion",
+                newName: "Like");
+        }
+    }
+}
--- LibraryBackend/Models/Opinion.cs
+++ LibraryBackend/Models/Opinion.cs
@@ -4,7 +4,7 @@ namespace LibraryBackend.Models
 {
   public class Opinion : BaseEntity
   {
-    public int? Like { get; set; } = 1;
+    public int? Rate { get; set; } = 1;
 
     public string? View { get; set; }
 
