03a7391f1a557ddd12b8c58a74075a097e92037a
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-13 10:17:36 +0200

    re-implemente  OpinionController action methods

--- LibraryBackend/Controllers/OpinionController.cs
+++ LibraryBackend/Controllers/OpinionController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryBackend.Models;
 using LibraryBackend.Data;
-
+using System.Linq.Expressions;
 
 
 namespace LibraryBackend.Controllers
@@ -16,129 +16,39 @@ namespace LibraryBackend.Controllers
   [ApiController]
   public class OpinionController : ControllerBase
   {
-
     private readonly IRepository<Opinion> _OpinionRepository;
-    private readonly string dateTimeFormat = "yyyy-MM-ddTHH:mm:ss";
-
     public OpinionController(IRepository<Opinion> opinionRepository)
     {
       _OpinionRepository = opinionRepository;
     }
 
     // GET: api/Opinion
-    /* [HttpGet]
-    public async Task<ActionResult<IEnumerable<Opinion>>> GetOpinion()
-    {
-        return await _context.Opinion.ToListAsync();
-    } */
-
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IEnumerable<Opinion>>> GetOpinion()
     {
-      var Opinions = await _OpinionRepository.GetAllAsync();
-      return Ok(Opinions);
+      /* var Opinions = await _OpinionRepository.GetAllAsync();
+      return Ok(Opinions); */
+      throw new NotImplementedException();
     }
     // GET: api/Opinion/5
-    [HttpGet("{id}")]
-    /* public ActionResult<Opinion> GetOpinionById(int id)
-    {
-        var opinion = _context.Opinion.Include(x => x.Book).FirstOrDefault(x => x.Id == id);
-
-        if (opinion == null)
-        {
-            return NotFound();
-        }
-
-        return opinion;
-    } */
-
+    [HttpGet("{bookId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<Book>> GetOpinionById(int id)
+    public async Task<ActionResult<IEnumerable<Opinion>>> GetOpinionsByBookId(int bookId)
     {
-      try
-      {
-        var opinion = await _OpinionRepository.GetByIdAsync(id);
-        if (opinion == null) return NotFound();
-        return Ok(opinion);
+      /* Expression<Func<Opinion, bool>> condition = opinion => opinion.BookId == bookId;
 
-      }
-      catch
+      var opinions = await _OpinionRepository.FindByConditionAsync(condition);
+
+      if (opinions == null || !opinions.Any())
       {
         return NotFound();
       }
-
+      return Ok(opinions); */
+      throw new NotImplementedException();
     }
 
-    // PUT: api/Opinion/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    /* [HttpPut("{id}")]
-    public async Task<IActionResult> PutOpinion(int id, Opinion opinion)
-    {
-      if (id != opinion.Id)
-      {
-        return BadRequest();
-      }
-
-      _context.Entry(opinion).State = EntityState.Modified;
-
-      try
-      {
-        await _context.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!OpinionExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
-      }
-
-      return NoContent();
-    } */
-
-    // POST: api/Opinion
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    /* [HttpPost]
-    public async Task<ActionResult<Opinion>> PostOpinion(OpinionAddRequest opinion)
-    {
-      _context.Opinion.Add(new Opinion
-      {
-        Like = opinion.Like,
-        BookId = opinion.BookId,
-        View = opinion.View,
-        userName = opinion.userName
-      });
-      await _context.SaveChangesAsync();
-
-      return CreatedAtAction("GetOpinion", new { id = opinion.OpinionId }, opinion);
-    } */
-
-    // DELETE: api/Opinion/5
-    /* [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteOpinion(int id)
-    {
-      var opinion = await _context.Opinion.FindAsync(id);
-      if (opinion == null)
-      {
-        return NotFound();
-      }
-
-      _context.Opinion.Remove(opinion);
-      await _context.SaveChangesAsync();
-
-      return NoContent();
-    } */
-
-    /* private bool OpinionExists(int id)
-    {
-      return _context.Opinion.Any(e => e.Id == id);
-    } */
   }
 }
