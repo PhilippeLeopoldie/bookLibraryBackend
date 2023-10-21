c18a0c73d97ee1d12cd53646a94b0a4cbe1b5035
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-22 00:47:12 +0200

    Migrate new field 'PostDate' to Opinion table

---
+++ LibraryBackend/Migrations/20231021222224_newFieldPostDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class newFieldPostDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostDate",
+                table: "Opinion",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostDate",
+                table: "Opinion");
+        }
+    }
+}
