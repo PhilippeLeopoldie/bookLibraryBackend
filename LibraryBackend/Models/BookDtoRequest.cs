f2d183bdc36e6189d1f143e2892f86544ef0e9c0
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-11-18 18:46:24 +0100

    Add ImageUrl attribute to BookDtoRequest in BookDtoRequest.cs

--- LibraryBackend/Models/BookDtoRequest.cs
+++ LibraryBackend/Models/BookDtoRequest.cs
@@ -3,7 +3,7 @@ namespace LibraryBackend.Models
   public class BookDtoRequest 
   {
     public string? Title {get; set;}
-
     public string? Author {get; set;} 
+    public string? ImageUrl { get; set; }
   }
 }
