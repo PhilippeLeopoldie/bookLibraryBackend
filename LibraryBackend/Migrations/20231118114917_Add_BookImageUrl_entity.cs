fa546fff95f4848ce0aa22bcd2278e20f98f7c95
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-11-18 19:17:40 +0100

    Migrate ImageUrl property to Book entity

---
+++ LibraryBackend/Migrations/20231118114917_Add_BookImageUrl_entity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_BookImageUrl_entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Book",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Book");
+        }
+    }
+}
