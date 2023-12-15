9e1c1a7f3e5e8f031677035a18574b83bdae48ff
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-15 10:52:16 +0100

    Migrate averageRate attribute to Book and change Rate  type to double in Opinion

---
+++ LibraryBackend/Migrations/20231215092333_AddAverageRateToBook.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAverageRateToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rate",
+                table: "Opinion",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "AverageRate",
+                table: "Book",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRate",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rate",
+                table: "Opinion",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+        }
+    }
+}
