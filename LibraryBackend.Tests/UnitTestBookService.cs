077c1677540c31d2c1b619bd659372d9ab0fd4f9
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-15 17:00:31 +0100

    Implement test for EditAverage in UnitTestBookService.cs

---
+++ LibraryBackend.Tests/UnitTestBookService.cs
@@ -0,0 +1,59 @@
+using Moq;
+using LibraryBackend.Models;
+using LibraryBackend.Data;
+
+namespace LibraryBackend.Tests
+{
+  public class UnitTestBookService
+  {
+    readonly IBookService _bookService;
+    readonly Mock<IRepository<Book>> _mockBookRepository;
+
+
+    public UnitTestBookService()
+    {
+      _mockBookRepository = new Mock<IRepository<Book>>();
+      _bookService = new BookService(_mockBookRepository.Object);
+    }
+
+
+    [Fact]
+
+    public async Task Should_Edit_Average_Rate_in_EditAverageRate()
+    {
+      // arrange
+      var average = 3.7;
+      var bookToUpdate = new Book
+      {
+        Id = 1,
+        Title = "title1",
+        Author = "author1",
+        AverageRate = 3.3,
+        Opinions = new List<Opinion>
+          {
+            new Opinion{View="View1", BookId = 1, Rate = 5},
+            new Opinion{View="View2", BookId = 1, Rate = 2},
+            new Opinion{View="View3", BookId = 1, Rate = 3},
+          
+          }
+      };
+      _mockBookRepository
+        .Setup(mockBookRepository => mockBookRepository.GetByIdAsync(bookToUpdate.Id))
+        .ReturnsAsync(bookToUpdate);
+
+      _mockBookRepository
+        .Setup(mockBookRepository => mockBookRepository.Update(bookToUpdate))
+        .ReturnsAsync(bookToUpdate);
+
+
+      // Act
+      var updatedBook = await _bookService.EditAverageRate(bookToUpdate.Id, average);
+
+      // Assert
+      Assert.NotNull(updatedBook);
+      Assert.Equal(average, updatedBook?.AverageRate);
+      _mockBookRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Once);
+      _mockBookRepository.Verify(repo => repo.Update(It.IsAny<Book>()), Times.Once);
+    }
+  }
+}
