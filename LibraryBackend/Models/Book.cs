21d22ba14221072e68cf75e1fc1dbcea2c0c2ebe
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-15 10:47:27 +0100

    Add AverageRate attribute to Book in Book.cs

--- LibraryBackend/Models/Book.cs
+++ LibraryBackend/Models/Book.cs
@@ -5,6 +5,7 @@ namespace LibraryBackend.Models
     public string? Title { get; set; }
     public string? Author { get; set; }
     public string? ImageUrl { get; set; }
+    public double? AverageRate { get; set;}
     public List<Opinion>? Opinions { get; set; }
   }
 }
