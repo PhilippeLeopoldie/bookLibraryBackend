89ee4338b7fdc7e36c3b88a113b55038cc36ef95
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-19 20:59:04 +0100

    Refactor: remove unnecessary properties in BookDtoResponse

--- LibraryBackend/Models/BookDtoResponse.cs
+++ LibraryBackend/Models/BookDtoResponse.cs
@@ -5,7 +5,5 @@ namespace LibraryBackend.Models;
 public class BookDtoResponse 
 {
 public Book? Book {get; set;}
-public int TotalBooksCount { get; set;} = 0;
-public int TotalPagesCount { get; set; } = 0;
 public string? RequestedAt {get; set;}
 }
