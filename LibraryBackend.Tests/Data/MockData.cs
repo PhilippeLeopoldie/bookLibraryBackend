ce8c19bb1b66db996c705d3ce3c60938e6e06e8f
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-23 00:02:26 +0100

    Test: Implement genreMockData method for testing

--- LibraryBackend.Tests/Data/MockData.cs
+++ LibraryBackend.Tests/Data/MockData.cs
@@ -1,113 +1,174 @@
 using LibraryBackend.Models;
 
-namespace LibraryBackend.Tests.Data 
+namespace LibraryBackend.Tests.Data; 
+
+
+public static class MockData 
 {
-
-  public static class MockData 
+public static List<Book> GetMockData ()
+{
+  return new List<Book>
+{
+  new Book
   {
-    public static List<Book> GetMockData ()
-    {
-      return new List<Book>
-    {
-      new Book
+    Id = 1,
+    Title = "title1",
+    Author = "author1",
+    Opinions = new List<Opinion>
       {
-        Id = 1,
-        Title = "title1",
-        Author = "author1",
-        Opinions = new List<Opinion>
-          {
-            new Opinion
-           {
-            View="View1",
-            BookId = 1,
-            Rate = 5
-           },
-           new Opinion
-           {
-            View="View2",
-            BookId = 1,
-            Rate = 3
-           },
-           new Opinion
-           {
-            View="View3",
-            BookId = 1,
-            Rate = 4
-           }
-          }
-      },
-      new Book
+        new Opinion
+       {
+        View="View1",
+        BookId = 1,
+        Rate = 5
+       },
+       new Opinion
+       {
+        View="View2",
+        BookId = 1,
+        Rate = 3
+       },
+       new Opinion
+       {
+        View="View3",
+        BookId = 1,
+        Rate = 4
+       }
+      }
+  },
+  new Book
+  {
+    Id = 2,
+    Title ="title2",
+    Author ="author2"
+  },
+  new Book
+  {
+    Id = 3,
+    Title ="title3",
+    Author ="author3"
+  },
+  new Book
+  {
+    Id = 4,
+    Title ="title4",
+    Author ="author4"
+  },
+  new Book
+  {
+    Id = 5,
+    Title = "title5",
+    Author = "author5",
+    Opinions = new List<Opinion>
       {
-        Id = 2,
-        Title ="title2",
-        Author ="author2"
-      },
-      new Book
-      {
-        Id = 3,
-        Title ="title3",
-        Author ="author3"
-      },
-      new Book
-      {
-        Id = 4,
-        Title ="title4",
-        Author ="author4"
-      },
-      new Book
-      {
-        Id = 5,
-        Title = "title5",
-        Author = "author5",
-        Opinions = new List<Opinion>
-          {
-            new Opinion
-           {
-            View="View1",
-            BookId = 5,
-            Rate = 5
-           },
-           new Opinion
-           {
-            View="View2",
-            BookId = 5,
-            Rate = 3
-           },
-           new Opinion
-           {
-            View="View3",
-            BookId = 5,
-            Rate = 4
-           }
-          }
-      },
-      new Book
-      {
-        Id = 6,
-        Title ="title6",
-        Author ="author6"
-      },
-      new Book
-      {
-        Id = 7,
-        Title ="title7",
-        Author ="author7"
-      },
-      new Book
-      {
-        Id = 8,
-        Title ="title8",
-        Author ="author8"
-      },
-      new Book
-      {
-        Id = 9,
-        Title ="title9",
-        Author ="author9"
-      },
+        new Opinion
+       {
+        View="View1",
+        BookId = 5,
+        Rate = 5
+       },
+       new Opinion
+       {
+        View="View2",
+        BookId = 5,
+        Rate = 3
+       },
+       new Opinion
+       {
+        View="View3",
+        BookId = 5,
+        Rate = 4
+       }
+      }
+  },
+  new Book
+  {
+    Id = 6,
+    Title ="title6",
+    Author ="author6"
+  },
+  new Book
+  {
+    Id = 7,
+    Title ="title7",
+    Author ="author7"
+  },
+  new Book
+  {
+    Id = 8,
+    Title ="title8",
+    Author ="author8"
+  },
+  new Book
+  {
+    Id = 9,
+    Title ="title9",
+    Author ="author9"
+  },
 
-    };
+};
 
-    }
-  }
+}
+
+public static List<Genre> GetGenreMockData()
+    {
+        return new List<Genre>
+        { 
+            new Genre
+            {
+                Id = 1,
+                Name= "genre1",
+                Books = new List<Book>
+                {
+                    new Book
+                    {
+                        Id= 1,
+                        Title= "title1Genre1",
+                        Author= "author1Genre1",
+                    },
+                    new Book
+                    {
+                        Id= 2,
+                        Title= "title2Genre1",
+                        Author= "author2Genre1",
+                    }
+                }
+            },
+            new Genre
+            {
+                Id = 2,
+                Name= "genre2",
+                Books = new List<Book>
+                {
+                    new Book
+                    {
+                        Id= 3,
+                        Title= "title3Genre2",
+                        Author= "author3Genre2",
+                    },
+                    new Book
+                    {
+                        Id= 4,
+                        Title= "title4Genre2",
+                        Author= "author4Genre2",
+                    }
+
+                }
+            },
+            new Genre
+            {
+                Id = 3,
+                Name= "genre3",
+                Books = new List<Book>
+                {
+                    new Book
+                    {
+                        Id= 5,
+                        Title= "title5Genre3",
+                        Author= "author5Genre3",
+                    }
+                }
+            }
+        };
+}
 }
