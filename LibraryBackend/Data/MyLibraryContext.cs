7cb6dafb65de488ef48768be98a5c894cc575a56
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-22 23:57:57 +0100

    Feature: Add Genre DbSet to DbContext

--- LibraryBackend/Data/MyLibraryContext.cs
+++ LibraryBackend/Data/MyLibraryContext.cs
@@ -16,6 +16,7 @@ using LibraryBackend.Models;
         public DbSet<Opinion> Opinion { get; set; }=default!;
 
         public DbSet<Book> Book { get; set; }=default!;
+        public DbSet<Genre> Genres { get; set; } =default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
