98d4621fa65da68275cede42631e36502b9874b2
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-23 00:08:59 +0100

    Feature: Implement GenreController with unimplemented methods for failing test first

---
+++ LibraryBackend/Controllers/GenreController.cs
@@ -0,0 +1,56 @@
+﻿using LibraryBackend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using LibraryBackend.Models;
+using LibraryBackend.Services;
+
+
+
+namespace LibraryBackend.Controllers;
+
+[Route("api/[controller]s")]
+[ApiController]
+public class GenreController : ControllerBase
+{
+    private readonly IRepository<Genre> _genreRepository;
+    private readonly IGenreService _genreService;
+
+    public GenreController (IRepository<Genre> genreRepository)
+    {
+        _genreRepository = genreRepository;
+    }
+
+    // GET: api/<GenreController>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<string>>> GetGenres()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/<GenreController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<GenreController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<GenreController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<GenreController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
