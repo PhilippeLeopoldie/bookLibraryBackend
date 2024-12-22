3f91433178453cf3f8c2382f4bad5a1f99fdf373
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-22 23:58:48 +0100

    Feature: implement IGenreService and GenreService

---
+++ LibraryBackend/Services/GenreService.cs
@@ -0,0 +1,22 @@
+﻿using LibraryBackend.Models;
+
+
+namespace LibraryBackend.Services;
+
+public class GenreService : IGenreService
+{
+    
+   
+    private readonly MyLibraryContext _libraryContext;
+
+    public GenreService( MyLibraryContext libraryContext)
+    {
+        
+        _libraryContext = libraryContext;
+    }
+
+    public Task<IEnumerable<Genre?>> ListOfGenreAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LibraryBackend/Services/IGenreService.cs
@@ -0,0 +1,8 @@
+﻿using LibraryBackend.Models;
+
+namespace LibraryBackend.Services;
+
+public interface IGenreService
+{
+    Task<IEnumerable<Genre?>> ListOfGenreAsync();
+}
