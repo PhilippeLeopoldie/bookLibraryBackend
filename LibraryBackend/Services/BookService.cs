aa736f34b518408a34c105f17fbf06e9836fa1a3
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-01-10 17:13:52 +0100

    Refactor: Extract getMostPopularBooks as helper fonction for better separation of concerns.

--- LibraryBackend/Services/BookService.cs
+++ LibraryBackend/Services/BookService.cs
@@ -6,50 +6,57 @@ namespace LibraryBackend.Services;
 
 public class BookService : IBookService
 {
-private readonly IRepository<Book> _bookRepository;
-private readonly MyLibraryContext _dbContext;
-public BookService(IRepository<Book> bookRepository, MyLibraryContext dbContext)
-{
-  _bookRepository = bookRepository;
-  _dbContext = dbContext;
-}
+    private readonly IRepository<Book> _bookRepository;
+    private readonly MyLibraryContext _dbContext;
+    public BookService(IRepository<Book> bookRepository, MyLibraryContext dbContext)
+    {
+        _bookRepository = bookRepository;
+        _dbContext = dbContext;
+    }
 
-// Top  books with the biggest number of review that has a rate >=3
-public virtual async Task<IEnumerable<Book>?> HighestAverageRate(int numberOfBooks)
-{
-  var books = await _dbContext.Book
-      .Include(book => book.Opinions)
-      .ToListAsync();
+    // Most popular books are those with the biggest number of reviews with a rate >=3
+    public IEnumerable<Book>? getMostPopularBooks(List<Book> books, int numberOfBooks)
+    {
+        var mostPopularBooks = books
+            .Where(book => book.Opinions != null && book.Opinions.Any(opinion => opinion.Rate >= 3))
+            .OrderByDescending(book => book.Opinions != null ?
+                book.Opinions.Count(opinion => opinion.Rate >= 3)
+                : 0)
+            .ThenByDescending(book => book.Opinions != null ?
+                book.Opinions.Where(opinion => opinion.Rate >= 3).Average(opinion => opinion.Rate)
+                : null)
+            .Take(numberOfBooks).ToList();
 
-  var topBook = books
-      .Where(book => book.Opinions != null && book.Opinions.Any(opinion => opinion.Rate >= 3))
-      .OrderByDescending(book => book.Opinions != null ?
-      book.Opinions.Count(opinion => opinion.Rate >= 3)
-      :0)
-      .ThenByDescending(book => book.Opinions != null ?
-       book.Opinions.Where(opinion => opinion.Rate >= 3).Average(opinion => opinion.Rate)
-       :null)
-      .Take(numberOfBooks).ToList();
+        return mostPopularBooks;
+    }
 
-  return topBook;
-}
+   
+    public virtual async Task<IEnumerable<Book>?> HighestAverageRate(int numberOfBooks)
+    {
+        var books = await _dbContext.Book
+            .Include(book => book.Opinions)
+            .ToListAsync();
+        var mostPopularBooks = getMostPopularBooks(books, numberOfBooks);
 
-public async Task<Book?> EditAverageRate(int bookId, double average)
-{
-  var book = await _bookRepository.GetByIdAsync(bookId);
+        return mostPopularBooks;
+    }
 
-  if (book == null)
-  {
-    return null;
-  }
-  book.AverageRate = average;
-  var updatedBook = await _bookRepository.Update(book);
-  return updatedBook;
-}
+    public async Task<Book?> EditAverageRate(int bookId, double average)
+    {
+        var book = await _bookRepository.GetByIdAsync(bookId);
 
-public virtual async Task<IEnumerable<Book?>> ListOfBooksAsync()
-{
-  var books = await _bookRepository.GetAllAsync();
-    return books;
-}
+        if (book == null)
+        {
+            return null;
+        }
+        book.AverageRate = average;
+        var updatedBook = await _bookRepository.Update(book);
+        return updatedBook;
+    }
+
+    public virtual async Task<IEnumerable<Book?>> ListOfBooksAsync()
+    {
+        var books = await _bookRepository.GetAllAsync();
+        return books;
+    }
 }
