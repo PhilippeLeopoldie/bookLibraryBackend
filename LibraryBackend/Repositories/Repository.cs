40d9ca13055ea574785044943aa2ad8b818665ff
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-01-11 19:38:28 +0100

    Refactor: Use _entities field instead of  context.Set<T> in GetByIdAsync and FindByConditionAsync

--- LibraryBackend/Repositories/Repository.cs
+++ LibraryBackend/Repositories/Repository.cs
@@ -6,47 +6,47 @@ namespace LibraryBackend.Repositories;
 
 public class Repository<T> : IRepository<T> where T : BaseEntity
 {
-private readonly MyLibraryContext _context = default!;
-private readonly DbSet<T> _entities;
+    private readonly MyLibraryContext _context = default!;
+    private readonly DbSet<T> _entities;
 
-public Repository(MyLibraryContext context)
-{
-  _context = context;
-  _entities = context.Set<T>();
-}
+    public Repository(MyLibraryContext context)
+    {
+        _context = context;
+        _entities = context.Set<T>();
+    }
 
-public virtual async Task<IEnumerable<T>> GetAllAsync()
-{
-  return await _entities.OrderByDescending(entity => entity.Id).ToListAsync();
-}
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _entities.OrderByDescending(entity => entity.Id).ToListAsync();
+    }
 
-public virtual async Task<T> Create(T entity)
-{
-  _entities.Add(entity);
-  await _context.SaveChangesAsync();
-  return entity;
-}
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await _entities.FirstOrDefaultAsync(x => x.Id == id);
+    }
 
-public virtual async Task Delete(T entityToDelete)
-{
-  _context.Remove(entityToDelete);
-  await _context.SaveChangesAsync();
-}
+    public virtual async Task<IEnumerable<T?>> FindByConditionAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _entities.Where(predicate).ToListAsync();
+    }
 
-public virtual async Task<T> Update(T entity)
-{
-  _context.Update(entity);
-  await _context.SaveChangesAsync();
-  return entity!;
-}
+    public virtual async Task<T> Create(T entity)
+    {
+        _entities.Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
 
-public virtual async Task<T?> GetByIdAsync(int id)
-{
-  return await _entities.FirstOrDefaultAsync(x => x.Id == id);
-}
+    public virtual async Task Delete(T entityToDelete)
+    {
+        _context.Remove(entityToDelete);
+        await _context.SaveChangesAsync();
+    }
 
-public virtual async Task<IEnumerable<T?>> FindByConditionAsync(Expression<Func<T, bool>> predicate)
-{
-  return await _context.Set<T>().Where(predicate).ToListAsync();
-}
+    public virtual async Task<T> Update(T entity)
+    {
+        _context.Update(entity);
+        await _context.SaveChangesAsync();
+        return entity!;
+    }
 }
