98d3a37224999e8b77d930c56d7d80200a8146c7
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-01-23 20:35:16 +0100

    feat: Update the return type of GetPaginatedBooksByGenreIdAsync

--- LibraryBackend/Services/IBookService.cs
+++ LibraryBackend/Services/IBookService.cs
@@ -7,6 +7,6 @@ public interface IBookService
     Task<IEnumerable<Book>?> GetBooksWithHighestAverageRate(int numberOfBooks);
     Task<Book?> EditAverageRate(int bookId, double average);
     Task<PaginationResult<Book>> GetListOfBooksWithOpinionsAsync(int page, int numberOfItemsPerPage);
-    Task<IEnumerable<Book?>> GetPaginatedBooksByGenreIdAsync(string listOfGenreId, int page, int ItemsPerPage);
+    Task<PaginationResult<Book>> GetPaginatedBooksByGenreIdAsync(string listOfGenreId, int page, int itemsPerPage);
     Task<IEnumerable<Book?>> GetBookByTitleOrAuthor(string titleOrAuthor);
 }
