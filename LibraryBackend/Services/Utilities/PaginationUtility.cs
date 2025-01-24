e2e91ae7a65feb5b0aef935b7e4b8eb510569f3c
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-01-24 23:32:18 +0100

    refactor: Remove commented-out code

--- LibraryBackend/Services/Utilities/PaginationUtility.cs
+++ LibraryBackend/Services/Utilities/PaginationUtility.cs
@@ -10,17 +10,7 @@ public class PaginationUtility<T> where T : BaseEntity
     [Range(1, 6, ErrorMessage = "Number of items per page must be between 1 and 6")]
     public int ItemsPerPage { get; set; }
 
-    //public int LimitNumberOfItemsPerPage { get; set; } = 6;
     public readonly string dateTimeFormat = "yyyy-MM-ddTHH:mm:ss";
-    
-    /*public virtual void PaginationValidation (int page, int itemsPerPage)
-    {
-        if (page <= 0) throw new ArgumentException("Number of page must be greater than 0");
-        if (itemsPerPage <= 0 || itemsPerPage > LimitNumberOfItemsPerPage)
-            throw new ArgumentException($"Number of items per page must be between 1 and {LimitNumberOfItemsPerPage}");
-    }*/
-
-
 
     public virtual void PaginatedItemsValidation (IEnumerable<T> paginatedItems,int page)
     {
@@ -33,12 +23,6 @@ public class PaginationUtility<T> where T : BaseEntity
     public virtual  PaginationResult<T> GetPaginationResult(IEnumerable<T> listOfPaginatedItems, int totalItems, int page, int ItemsPerPage)
     {
         var totalPages = (int)Math.Ceiling((double)totalItems / ItemsPerPage);
-
-        /*if (page > totalPages && page > 0)
-            throw new ArgumentException(
-                $"Page {page} does not exist, the last page is {totalPages}"
-            );*/
-
         return new PaginationResult<T>(
             listOfPaginatedItems,
             totalItems,
