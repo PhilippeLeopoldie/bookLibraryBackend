9e6de2a770e0f3be4aad1356b13bff725a79baaf
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-02 23:47:51 +0100

    feat: Implement StoryService

---
+++ LibraryBackend/Services/StoryService.cs
@@ -0,0 +1,24 @@
+﻿using OpenAI.Chat;
+
+namespace LibraryBackend.Services;
+
+public class StoryService : IStoryService
+{
+    private readonly string _apiKey;
+    private readonly string _modelNames;
+
+    public StoryService(IConfiguration configuration)
+    {
+        _apiKey = configuration["OPENAI_API_KEY"] ??
+            Environment.GetEnvironmentVariable("OPENAI_API_KEY") ??
+            throw new ArgumentNullException(nameof(configuration));
+        _modelNames = "gpt-3.5-turbo";
+    }
+
+    public async Task<string> GenerateAIStoryAsync(string prompt)
+    {
+        var client = new ChatClient(_modelNames, _apiKey);
+        var response = await client.CompleteChatAsync(prompt);
+        return response.Value.Content[0].Text;
+    }
+}
